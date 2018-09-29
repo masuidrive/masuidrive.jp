3f3472dc4de3dce04e23e9787ad62863e7d8a5e3
MASUI Yuichiro <masui@masuidrive.jp>
2018-09-29 22:07:04 +0900

    Fixed something

--- builder/resume/build-css.sh
+++ builder/resume/build-css.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export PATH=./node_modules/.bin/:$PATH
+export PATH=/usr/src/app/node_modules/.bin/:$PATH
 
 mkdir -p tmp dist
 
 sass styles/index.scss tmp/resume.css -I node_modules &&
 uncss tmp/resume.html > tmp/resume.min.css &&
 cp tmp/resume.min.css tmp/resume.css &&
-inliner --noimages tmp/resume.html > dist/resume.html ||
-exit 1;
+(cd tmp && inline-css -i resume.html -o resume.min.html) &&
+htmlminify tmp/resume.min.html -o dist/resume.html || exit 1;
--- builder/top/build-css.sh
+++ builder/top/build-css.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export PATH=./node_modules/.bin/:$PATH
+export PATH=/usr/src/app/node_modules/.bin/:$PATH
 
 mkdir -p tmp/styles dist
 
 sass styles/index.scss tmp/styles/index.css -I node_modules &&
 uncss tmp/index.html > tmp/index.min.css &&
-cp tmp/index.min.css tmp/index.css &&
-inliner --noimages tmp/index.html > dist/index.html || exit 1;
+cp tmp/index.min.css tmp/styles/index.css &&
+(cd tmp && inline-css -i index.html -o index.min.html) &&
+htmlminify tmp/index.min.html -o dist/index.html || exit 1;
